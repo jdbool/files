92e0d001fc3ead4f359e4d5c8527e0065e561d1f
jdb <44955015+jdbool@users.noreply.github.com>
2019-12-15 18:31:23 -0800

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # files
- Simple file hosting server for Node.js
+Simple file hosting server for Node.js. Stores file information in an SQLite database and uploads in the file system.
+
+# Usage
+There's no way to upload via a browser yet, it's designed for tools like [ShareX](https://getsharex.com/).
+
+## Uploading
+**POST /upload**
+
+The `Authorization` header has to be a valid token, and the file should be called `file` (multipart/form-data).
+
+Response (if OK, else there will be an `error` object):
+```js
+{
+	// 7 character long file ID
+	"id": String,
+	// MIME type
+	"type": String,
+	// Size in bytes
+	"size": Number,
+	// The IP you uploaded from
+	"ip": String,
+	// 64 character long key that can be used to delete the file
+	"deleteKey": String,
+	// MD5 file hash
+	"hash": String,
+	// Name of the file you uploaded
+	"originalName": String
+}
+```
+
+The file will then be accessible at `GET /{id}`.
+
+## Deleting
+**GET /delete/{id}/{deleteKey}**
+
+Uses GET so the link can be opened in a browser. If the request doesn't accept HTML, and OK response would be:
+```js
+{
+	"deleted": true,
+	"type": String,
+	"size": Number,
+	"ip": String,
+	// How many times the file was sent to a human (unless they lied)
+	"hits": Number,
+	// How many times it was sent to a bot
+	"botHits": Number
+}
+```
+
+## Admin
+**GET /admin** in a browser
+
+It'll prompt for a username and password and show all uploaded files.
+
+# Configuration
+`config.json` has to be created. Example:
+```js
+{
+	"port": 3020,
+	"adminUsers": {
+		"username": "password",
+		...
+	},
+	"tokens": [
+		"alonguniquethinghere",
+		...
+	]
+}
+```
---
+++ app.js
@@ -0,0 +1,288 @@
+const config = require('./config.json');
+
+const path = require('path');
+const fs = require('fs').promises;
+
+const prettySize = require('prettysize');
+const chalk = require('chalk');
+const Sequelize = require('sequelize');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const rateLimit = require('express-rate-limit');
+const basicAuth = require('express-basic-auth');
+const handlebars = require('express-handlebars');
+const randomString = require('randomstring');
+const send = require('send');
+const hasha = require('hasha');
+
+const db = new Sequelize({
+	dialect: 'sqlite',
+	storage: path.join(__dirname, 'files.db'),
+	logging: false
+});
+
+const File = db.define('file', {
+	id: {
+		type: Sequelize.STRING(7),
+		primaryKey: true
+	},
+	type: Sequelize.STRING,
+	size: Sequelize.INTEGER,
+	hits: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+	botHits: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+	ip: Sequelize.STRING,
+	deleteKey: Sequelize.STRING(64),
+	hash: Sequelize.STRING(64),
+	originalName: Sequelize.STRING
+});
+
+const app = express();
+app.set('trust proxy', 'loopback');
+
+app.use(fileUpload({
+	useTempFiles: true,
+	tempFileDir: process.platform == 'win32' ? 'C:/temp/' : '/tmp/'
+}));
+
+app.engine('handlebars', handlebars.create({
+	helpers: {
+		prettySize
+	},
+	defaultLayout: false
+}).engine);
+app.set('view engine', 'handlebars');
+app.enable('view cache');
+app.set('views', path.join(__dirname, 'views'));
+
+const uploadsPath = id => path.join(__dirname, 'uploads', id);
+
+const problem = (req, res, error) => {
+	res.status(error.status);
+	if (req.accepts('html')) {
+		res.render('error', error);
+	} else {
+		// Object with a property called 'error'
+		res.json({
+			error
+		});
+	}
+};
+
+app.get('/robots.txt', (req, res) => {
+	res.set('Content-Type', 'text/plain').send('User-agent: *\nDisallow: /');
+});
+
+app.get('/admin', basicAuth({
+	users: config.adminUsers,
+	challenge: true,
+	realm: 'files-admin'
+}), async (req, res) => {
+	const files = await File.findAll();
+	res.render('admin', {
+		files
+	});
+});
+
+app.post('/upload', rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 10,
+	message: {
+		error: {
+			status: 429,
+			title: 'Too Many Requests',
+			detail: 'You are uploading too many files'
+		}
+	}
+}), async (req, res) => {
+	const token = req.get('Authorization');
+
+	if (!config.tokens.includes(token)) {
+		problem(req, res, {
+			status: 401,
+			title: 'Unauthorized',
+			detail: 'No authorized token given'
+		});
+		return;
+	}
+
+	const upload = req.files.file;
+	
+	if (!upload) {
+		problem(req, res, {
+			status: 400,
+			title: 'Bad Request',
+			detail: 'No file given'
+		});
+		return;
+	}
+
+	try {
+		let id;
+
+		// eslint-disable-next-line no-constant-condition
+		while (true) {
+			id = randomString.generate(7);
+			const used = await File.findByPk(id);
+			if (!used) break;
+		}
+
+		const hash = await hasha.fromFile(upload.tempFilePath, { algorithm: 'md5' });
+
+		await fs.rename(upload.tempFilePath, uploadsPath(id));
+
+		const file = await File.create({
+			id,
+			type: upload.mimetype,
+			size: upload.size,
+			ip: req.ip,
+			deleteKey: randomString.generate(64),
+			hash,
+			originalName: upload.name
+		});
+	
+		res.json({
+			id: file.id,
+			type: file.type,
+			size: file.size,
+			ip: file.ip,
+			deleteKey: file.deleteKey,
+			hash: file.hash,
+			originalName: file.originalName
+		});
+	
+		console.log(chalk.green('[Upload] ') + `${file.id} - ${file.type} by ${file.ip} (${token.substr(0, 8)}...)`);
+	} catch (err) {
+		problem(req, res, {
+			status: 500,
+			title: 'Internal Server Error',
+			detail: err
+		});
+		console.log(chalk.red('[Upload] ') + `Unable to upload:\n\t${err}`);
+	}
+});
+
+app.get('/:id', rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: {
+		error: {
+			status: 429,
+			title: 'Too Many Requests',
+			detail: 'You are requesting too many files'
+		}
+	}
+}), async (req, res) => {
+	const file = await File.findByPk(req.params.id);
+	if (!file) {
+		problem(req, res, {
+			status: 404,
+			title: 'Not Found',
+			detail: 'No file with that ID'
+		});
+		return;
+	}
+
+	res.set('Content-Type', file.type);
+	res.set('Cache-Control', 'public, max-age=31536000');
+
+	if (!req.get('Range')) {
+		if (!req.get('User-Agent') || !req.get('Accept') || req.get('User-Agent').toLowerCase().includes('bot')) {
+			await file.increment('botHits', { by: 1 });
+			console.log(chalk.cyan('[Bot Hit] ') + `${file.id} - ${req.ip} - ${req.get('User-Agent')}`);
+		} else {
+			await file.increment('hits', { by: 1 });
+			console.log(chalk.blue('[Hit] ') + `${file.id} - ${req.ip}`);
+		}
+	}
+
+	try {
+		send(req, uploadsPath(file.id)).pipe(res);
+	} catch (err) {
+		problem(req, res, {
+			status: 500,
+			title: 'Internal Server Error',
+			detail: 'Could not send file'
+		});
+	}
+});
+
+app.get('/delete/:id/:deleteKey', rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 10,
+	message: {
+		error: {
+			status: 429,
+			title: 'Too Many Requests',
+			detail: 'You are deleting too many files'
+		}
+	}
+}), async (req, res) => {
+	const file = await File.findOne({
+		where: req.params
+	});
+
+	if (!file) {
+		problem(req, res, {
+			status: 404,
+			title: 'Not Found',
+			detail: 'No file with that ID or deleteKey'
+		});
+		return;
+	}
+
+	if (req.accepts('html')) {
+		res.render('deleted');
+	} else {
+		res.json({
+			deleted: true,
+			type: file.type,
+			size: file.size,
+			ip: file.ip,
+			hits: file.hits,
+			botHits: file.botHits
+		});
+	}
+
+	await file.destroy();
+	
+	try {
+		await fs.unlink(uploadsPath(file.id));
+		console.log(chalk.yellow('[Delete] ') + file.id);
+	} catch (err) {
+		console.log(chalk.red('[Delete] ') + `${file.id}, unable to erase data\n\t${err}`);
+	}
+});
+
+app.get('/', (req, res, next) => {
+	if (req.accepts('html')) {
+		res.render('index');
+	} else {
+		problem(req, res, {
+			status: 600,
+			title: 'Gray Goo',
+			detail: 'https://en.wikipedia.org/wiki/Gray_goo'
+		});
+	}
+});
+
+app.use((req, res) => {
+	problem(req, res, {
+		status: 404,
+		title: 'Not Found',
+		detail: 'Endpoint does not exist'
+	});
+});
+
+(async () => {
+	await db.sync();
+
+	app.listen(config.port, () => {
+		console.log(`HTTP listening on port ${config.port}`);
+	});
+})();
